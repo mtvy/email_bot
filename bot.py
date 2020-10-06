368eabac2b1c3f7ea2b9dc0ceef4e0edc0de5f83
mtvy <m.d.prudnik@gmail.com>
2020-10-06 21:43:09 +0300

    007

--- bot.py
+++ bot.py
@@ -214,16 +214,17 @@ def mailSet(user, password, person_id):
     except Exception as e:
         latest_email_id = "b'0'"
         print(e)
-    check_last_email_id = takeLastEmailId(latest_email_id, person_id, user)
-    if check_last_email_id != 1: return
-    result, data = con.fetch(latest_email_id, ' (RFC822) ')
-    raw = email.message_from_bytes(data[0][1])
-    show_bottom = email.utils.parseaddr(raw['From'])
     try:
+        check_last_email_id = takeLastEmailId(latest_email_id, person_id, user)
+        if check_last_email_id != 1: return
+        result, data = con.fetch(latest_email_id, ' (RFC822) ')
+        raw = email.message_from_bytes(data[0][1])
+        show_bottom = email.utils.parseaddr(raw['From'])
         email_body_text = get_body(raw).decode("utf-8")
     except Exception as e:
-        email_body_text = get_body(raw)
-        print(e)
+        print('Error making normal text by ' + person_id + '!')
+        return
+
     check_ln = 0
     contents = []
     name_cont = []
